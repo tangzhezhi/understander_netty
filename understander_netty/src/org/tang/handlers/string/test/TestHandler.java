705f08bc958285907d718b8d4074bd4f2b010726
tangzhezhi <330882908@qq.com>
2014-08-12 16:47:11 +0800

    Signed-off-by: tangzhezhi <330882908@qq.com>

--- understander_netty/src/org/tang/handlers/string/test/TestHandler.java
+++ understander_netty/src/org/tang/handlers/string/test/TestHandler.java
@@ -3,16 +3,12 @@ package org.tang.handlers.string.test;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.ChannelHandler.Sharable;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.tang.dto.string.test.MsgDTO;
-import org.tang.entity.test.Msg;
-import org.tang.handlers.ServerHandler;
 import org.tang.service.string.test.TestService;
 
 import com.google.gson.Gson;
@@ -42,8 +38,15 @@ public class TestHandler extends SimpleChannelInboundHandler<String>{
         	
         	MsgDTO dto = gson.fromJson(msg, MsgDTO.class);
         	
-        	testService.saveMsg(dto);
-        	
+        	int result = testService.saveMsg(dto);
+        	String remsg = "";
+        	if(result!=0){
+        		remsg = "sucess";
+        	}
+        	else{
+        		remsg = "false";
+        	}
+        	ctx.writeAndFlush(remsg);
         }
 	}
 
